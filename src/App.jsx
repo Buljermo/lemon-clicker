5390dc1bb80038f15739f04fb4860dfdf9a84111
Dmitrii Popov <bulpopov@gmail.com>
2023-05-25 09:59:04 +0300

    lisää Header-komponentin

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Lemon from './components/Lemon';
 import Balance from './components/Balance';
 import Booster from './components/Booster';
+import Header from './components/Header';
 
 
 function App() {
@@ -9,12 +10,14 @@ function App() {
   return (
     <>
       <div>
+        <Header>lemon clicker</Header>
         <Balance total="157" />
         <Lemon />
         <Booster value="3.2" />
       </div>  
     </>
   )
+
 }
 
 export default App
