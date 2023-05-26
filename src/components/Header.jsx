15fb89575abdad76275fcb64bad04e939a578c8c
DMITRII POPOV <bulpopov@gmail.com>
2023-05-26 18:37:05 +0300

    muuttaa Header-komponentin toiminnan

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,21 @@
+import iconLemon from '../assets/lemon.svg';
+
 function Header(props) {
 
+  if (props.hasOwnProperty("balance")) {
+    return (
+      <div className="header header_sub">
+        <h1>{props.children}</h1>
+        <div>{props.balance} <img src={iconLemon}/></div>
+      </div>
+    );
+  } else {
     return (
       <div className="header">
         <h1>{props.children}</h1>
       </div>
     );
-  
   }
-  
-  export default Header;
-  
+}
+
+export default Header;
