a4db6289af48dbeb365055c5160c6c87d616c9c3
DMITRII POPOV <bulpopov@gmail.com>
2023-05-31 10:49:51 +0300

    lisää tuotelistan

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
 import './App.css'
 import AppRouter from './components/AppRouter';
 import { useState } from 'react';
+import items from './config/items.js';
+
 
 
 function App() {
+  // Luodaan tilamuuttuja, johon tallennetaan tuotelista.
+  const [storeitems,setStoreitems] = useState(items);
   // Luodaan tilamuuttuja, johon tallennetaan pelin laskennalliset tiedot.
   const [stats, setStats] = useState({clicks: 0, balance: 0, increase: 1, itemstobuy: 0});
   const handleClick = () => {
-    // Tehdään kopio stats-tilamuuttujasta.
-    let newstats = {...stats}
-    // Kasvataan sitruunoiden määrää kasvatusarvolla.
-    newstats.balance = newstats.balance + newstats.increase;
-    // Tallennetaan päivitetty stats-muuttuja.
-    setStats(newstats); 
+  // Tehdään kopio stats-tilamuuttujasta.
+  let newstats = {...stats}
+  // Kasvataan sitruunoiden määrää kasvatusarvolla.
+  newstats.balance = newstats.balance + newstats.increase;
+  // Tallennetaan päivitetty stats-muuttuja.
+  setStats(newstats); 
   }
 
   return (
-    <AppRouter stats={stats} handleClick={handleClick} />
+    <AppRouter stats={stats} 
+               storeitems={storeitems} 
+               handleClick={handleClick} />
   )
 }
 
